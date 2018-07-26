2f882d91ce97ba8f0740f6e00083795f57394a41
D000M <georgislawow@gmail.com>
2018-07-26 18:38:52 +0300

    First Commit

---
+++ UnderWorldRun/main.cpp
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.cpp
+ * Author: DooM
+ *
+ * Created on July 26, 2018, 6:35 PM
+ */
+
+#include <cstdlib>
+#include <iostream>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    
+    std::cout << "Just checking desktop commiting.\n";
+    
+    return 0;
+}
+
