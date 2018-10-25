d84105e187b6612d852deac075711e8ae376f5dd
Georgi Slavov <georgi.slavov@egt-bg.com>
2018-10-25 17:17:32 +0300

    little fix for the calculator

--- JavaNotes/src/myGUI/Calculator.java
+++ JavaNotes/src/myGUI/Calculator.java
@@ -8,6 +8,8 @@ package myGUI;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import javax.swing.BorderFactory;
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -18,7 +20,7 @@ import javax.swing.JTextField;
  *
  * @author default
  */
-public class Calculator extends JPanel implements ActionListener{
+public class Calculator extends JPanel implements ActionListener, KeyListener{
     
     public static void runCalculator() {
         JFrame window = new JFrame();
@@ -59,7 +61,7 @@ public class Calculator extends JPanel implements ActionListener{
             buttons[i].addActionListener(this);
             buttonPanel.add(buttons[i]);
         }
-        
+        inputField.addKeyListener(this);
         inputField.setBounds(20, 30, 280, 30);
         buttonPanel.setBounds(5, 80, 310, 260);
         
@@ -207,5 +209,21 @@ public class Calculator extends JPanel implements ActionListener{
         else {
             secondNum = Double.parseDouble(result);
         }
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+        char c = e.getKeyChar();
+        if(!(Character.isDigit(c) || c == KeyEvent.VK_BACK_SPACE || c == KeyEvent.VK_DELETE )) {
+            e.consume();
+        }
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
     }
 }
